6540eaed179da1f71c0fbb46e44af50f007f0757
Robert Sainton <robertsainton@outlook.com>
2022-08-17 02:49:46 -0400

    assignment completed

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const bookUrl = "http://localhost:3000/books"
-    
+
+    const newUsername = "Robb"
       fetch(bookUrl)
     .then(function(response){ return response.json() })
     .then(function(data){
@@ -18,20 +19,47 @@ document.addEventListener("DOMContentLoaded", function() {
              unlikeBtn = document.createElement("button")
             img.src = book.img_url 
             showPanel.appendChild(img)
-            const newUsername = "Robb"
             ul.innerHTML = book.description
             showPanel.appendChild(ul)
            
             let usersList = book.users
             usersList.forEach( (data)=> {
+
+              if (data.username === "Robb") {
+                const li = document.createElement('li')
+                li.setAttribute('id', newUsername);
+                li.innerHTML = newUsername
+                showPanel.appendChild(li);
+              }
+              else {
                 const li = document.createElement('li')
                 li.innerHTML = data.username
                 showPanel.appendChild(li)
+              }
             })
            btn.innerHTML = "LIKE"
            showPanel.appendChild(btn)
 
-            btn.onclick = function () {
+            btn.onclick = function() {
+              const item = document.getElementById(newUsername)
+              if (item !== null) {
+                // let value = newUsername
+                //    newUsersList = usersList.filter(function(item) {
+                //   return item !== value
+             //})
+             usersList.pop()
+              console.log(usersList)
+             //console.log(newUsersList)
+                removeUser(usersList)
+              }
+              else {addUser()}
+            }
+            
+            
+            
+            function addUser () {
+
+             
               
                   showPanel.removeChild(showPanel.lastChild);
 
@@ -43,17 +71,26 @@ document.addEventListener("DOMContentLoaded", function() {
                         body: JSON.stringify({ users : [...usersList, {username:newUsername}]  })
                 })
                 const li = document.createElement('li')
-                li.setAttribute('id', newUsername.value);
+                li.setAttribute('id', newUsername);
                 li.innerHTML = newUsername
                 showPanel.appendChild(li)
                 unlikeBtn.innerHTML = "UNLIKE"
                 showPanel.appendChild(unlikeBtn)
             }
 
-            unlikeBtn.onclick = function () {
+            unlikeBtn.onclick = () => {
+            //   newUsersList = usersList.filter(function(item) {
+            //     return item !== newUsername
+            // })
+              console.log(usersList)
+              removeUser(usersList)}
+            
+            
+            function removeUser (usersList) {
               
                 showPanel.removeChild(showPanel.lastChild);
-                
+               
+                console.log(usersList)
 
               fetch ((bookUrl + `/${book.id}`) , {
                   method: 'PATCH',
@@ -62,7 +99,7 @@ document.addEventListener("DOMContentLoaded", function() {
                       },
                       body: JSON.stringify({ users : [...usersList]  })
               })
-              const item = document.getElementById(newUsername.value);
+              const item = document.getElementById(newUsername);
               showPanel.removeChild(item);
               btn.innerHTML = "LIKE"
            showPanel.appendChild(btn)
